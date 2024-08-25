f238e6d299ccef8442ce01ff33a27026b5f8321f
NIVAS-kr <20bcs1180@cuchd.in>
2024-08-25 14:46:11 +0530

    Removed deprecated MongoDB options

--- server/db.js
+++ server/db.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // Deprecated options removed
     });
     console.log('MongoDB connected:', conn.connection.host);
   } catch (error) {
     console.error('MongoDB connection error:', error);
-    process.exit(1);
+    process.exit(1); // Exit process with failure
   }
 };
 
